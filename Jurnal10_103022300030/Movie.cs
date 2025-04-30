88926bcd46364c50cab58145e56e59ca37050e62
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 16:26:53 +0700

    add MoviesMain

---
+++ Jurnal10_103022300030/Movie.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+
+namespace Jurnal10_103022300030
+{
+    [ApiController]
+    [Route("apu/[controller]")]
+    public class Movie
+    {
+        
+        private static List<Movie> TitleList = new List<Movie>()
+        {
+            new Movie("The Shawshank Redemption", "Frank Darabont", "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion."),
+            new Movie("The Godfather", "", ""),
+            new Movie("The Dark Knight", "", "")
+        };
+    [HttpGet]
+
+    public 
+
+    }
+}
---
+++ Jurnal10_103022300030/MoviesMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Jurnal10_103022300030
+{
+    public class Movies
+    {
+        public string Title { get; set; }
+        public string director { get; set; }
+
+        public static List<string> stars { get; set; }
+        public string description { get; set; }
+
+        public Movies(string Title ) {
+    }
+}
